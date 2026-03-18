8b465e9059d976f95f4faea84cc6b0fe245b14c2
Hao Liang <hflsforeverhao@gmail.com>
2026-03-18 16:46:35 -0400

    Trying with MCP resources

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -43,7 +43,23 @@ def edit_docs(
     docs[doc_id] = docs[doc_id].replace(old_string, new_string)
 
 # TODO: Write a resource to return all doc id's
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
 # TODO: Write a resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    return docs[doc_id]
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
